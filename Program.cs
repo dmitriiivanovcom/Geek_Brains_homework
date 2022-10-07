002be3fe3d08d3aec5d28dab9845ae4c792a50f9
Dmitrii Ivanov <114752708+dmitriiivanovcom@users.noreply.github.com>
2022-10-07 19:29:22 +0300

    example one

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// first example
+/*
+Console.WriteLine("add number");
+double numberOne = double.Parse(Console.ReadLine());
+Console.WriteLine("add another number");
+double numberTwo = double.Parse(Console.ReadLine());
+if(numberOne > numberTwo)
+    {
+        Console.WriteLine("first number is bigger");
+    }
+if(numberOne < numberTwo)
+    {
+        Console.WriteLine("second number is bigger");
+    }
+if(numberOne == numberTwo)
+    {
+        Console.WriteLine("its equals");
+    }
+    */
+
+    
